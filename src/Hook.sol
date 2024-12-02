c1d83b1181dc717271d7bf7b7a1b622b5f9ed696
philipmoniaga <philip.moniaga@gmail.com>
2024-12-02 22:19:29 +0700

    change lending to aave

--- src/Hook.sol
+++ src/Hook.sol
@@ -19,12 +19,7 @@ import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol
 import {SqrtPriceMath} from "v4-core/libraries/SqrtPriceMath.sol";
 import {FullMath} from "v4-core/libraries/FullMath.sol";
 import {PoolId} from "v4-core/types/PoolId.sol";
-
-interface ILend {
-    function deposit(address _token, uint256 _amount) external;
-    function withdraw(address _token, uint256 _amount) external;
-    function balance(address user, address token) external view returns (uint256);
-}
+import {IPool} from "./interfaces/IPool.sol";
 
 contract Hook is BaseHook {
     using CurrencyLibrary for Currency;
@@ -42,7 +37,7 @@ contract Hook is BaseHook {
 
     error PoolNotInitialized();
 
-    ILend public lendingProtocol;
+    IPool public lendingProtocol;
 
     int24 public tickLower;
     int24 public tickUpper;
@@ -54,11 +49,8 @@ contract Hook is BaseHook {
     mapping(address user => uint256 shares) public token0Shares;
     mapping(address user => uint256 shares) public token1Shares;
 
-    constructor(
-        IPoolManager _manager,
-        address _aavePool
-    ) BaseHook(_manager) {
-        lendingProtocol = ILend(_aavePool);
+    constructor(IPoolManager _manager, address _aavePool) BaseHook(_manager) {
+        lendingProtocol = IPool(_aavePool);
     }
 
     function getHookPermissions()
@@ -91,23 +83,49 @@ contract Hook is BaseHook {
         // Mint shares to user
         // Deposit tokens to lending protocol
         if (params.amount0 > 0) {
-            IERC20(Currency.unwrap(params.key.currency0)).transferFrom(msg.sender, address(this), params.amount0);
-            uint256 value = lendingProtocol.balance(address(this), Currency.unwrap(params.key.currency0));
-            uint256 shares = (params.amount0 * totalToken0Shares) / value;
+            IERC20(Currency.unwrap(params.key.currency0)).transferFrom(
+                msg.sender,
+                address(this),
+                params.amount0
+            );
+            uint256 amount0 = IERC20(Currency.unwrap(params.key.currency0))
+                .balanceOf(address(this));
+            uint256 shares = (params.amount0 * totalToken0Shares) / amount0;
             token0Shares[msg.sender] += shares;
             totalToken0Shares += shares;
-            IERC20(Currency.unwrap(params.key.currency0)).approve(address(lendingProtocol), params.amount0);
-            lendingProtocol.deposit(Currency.unwrap(params.key.currency0), params.amount0);
+            IERC20(Currency.unwrap(params.key.currency0)).approve(
+                address(lendingProtocol),
+                params.amount0
+            );
+            lendingProtocol.supply(
+                Currency.unwrap(params.key.currency0),
+                params.amount0,
+                address(this),
+                0
+            );
         }
 
         if (params.amount1 > 0) {
-            IERC20(Currency.unwrap(params.key.currency1)).transferFrom(msg.sender, address(this), params.amount1);
-            uint256 value = lendingProtocol.balance(address(this), Currency.unwrap(params.key.currency1));
-            uint256 shares = (params.amount1 * totalToken1Shares) / value;
+            IERC20(Currency.unwrap(params.key.currency1)).transferFrom(
+                msg.sender,
+                address(this),
+                params.amount1
+            );
+            uint256 amount1 = IERC20(Currency.unwrap(params.key.currency1))
+                .balanceOf(address(this));
+            uint256 shares = (params.amount1 * totalToken1Shares) / amount1;
             token1Shares[msg.sender] += shares;
             totalToken1Shares += shares;
-            IERC20(Currency.unwrap(params.key.currency1)).approve(address(lendingProtocol), params.amount1);
-            lendingProtocol.deposit(Currency.unwrap(params.key.currency1), params.amount1);
+            IERC20(Currency.unwrap(params.key.currency1)).approve(
+                address(lendingProtocol),
+                params.amount1
+            );
+            lendingProtocol.supply(
+                Currency.unwrap(params.key.currency1),
+                params.amount1,
+                address(this),
+                0
+            );
         }
     }
 
@@ -116,22 +134,40 @@ contract Hook is BaseHook {
         // Burn shares from user
         // Transfer tokens to user
         if (params.amount0 > 0) {
-            uint256 value = lendingProtocol.balance(address(this), Currency.unwrap(params.key.currency0));
-            uint256 shares = (params.amount0 * totalToken0Shares) / value;
+            uint256 amount0 = IERC20(
+                getATokenAddress(Currency.unwrap(params.key.currency0))
+            ).balanceOf(address(this));
+            uint256 shares = (params.amount0 * totalToken0Shares) / amount0;
             token0Shares[msg.sender] -= shares;
             totalToken0Shares -= shares;
-            lendingProtocol.withdraw(Currency.unwrap(params.key.currency0), params.amount0);
-            IERC20(Currency.unwrap(params.key.currency0)).transfer(msg.sender, params.amount0);
+            lendingProtocol.withdraw(
+                Currency.unwrap(params.key.currency0),
+                params.amount0,
+                address(this)
+            );
+            IERC20(Currency.unwrap(params.key.currency0)).transfer(
+                msg.sender,
+                params.amount0
+            );
         }
 
         if (params.amount1 > 0) {
-            uint256 value = lendingProtocol.balance(address(this), Currency.unwrap(params.key.currency1));
-            uint256 shares = (params.amount1 * totalToken1Shares) / value;
+            uint256 amount1 = IERC20(
+                getATokenAddress(Currency.unwrap(params.key.currency1))
+            ).balanceOf(address(this));
+            uint256 shares = (params.amount1 * totalToken1Shares) / amount1;
             token1Shares[msg.sender] -= shares;
             totalToken1Shares -= shares;
-            lendingProtocol.withdraw(Currency.unwrap(params.key.currency1), params.amount1);
-            IERC20(Currency.unwrap(params.key.currency1)).transfer(msg.sender, params.amount1);
-        }     
+            lendingProtocol.withdraw(
+                Currency.unwrap(params.key.currency1),
+                params.amount1,
+                address(this)
+            );
+            IERC20(Currency.unwrap(params.key.currency1)).transfer(
+                msg.sender,
+                params.amount1
+            );
+        }
     }
 
     function beforeSwap(
@@ -151,17 +187,39 @@ contract Hook is BaseHook {
 
         if (sender == EL_AVS) {
             (tickLower, tickUpper) = abi.decode(hookData, (int24, int24));
-            return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+            return (
+                this.beforeSwap.selector,
+                BeforeSwapDeltaLibrary.ZERO_DELTA,
+                0
+            );
         }
         // Withdraw from lending protocol
-        uint256 amount0 = lendingProtocol.balance(address(this), Currency.unwrap(key.currency0));
-        lendingProtocol.withdraw(Currency.unwrap(key.currency0), amount0);
-        uint256 amount1 = lendingProtocol.balance(address(this), Currency.unwrap(key.currency1));
-        lendingProtocol.withdraw(Currency.unwrap(key.currency1), amount1);
+        uint256 amount0 = IERC20(
+            getATokenAddress(Currency.unwrap(key.currency0))
+        ).balanceOf(address(this));
+        lendingProtocol.withdraw(
+            Currency.unwrap(key.currency0),
+            amount0,
+            address(this)
+        );
+        uint256 amount1 = IERC20(
+            getATokenAddress(Currency.unwrap(key.currency1))
+        ).balanceOf(address(this));
+        lendingProtocol.withdraw(
+            Currency.unwrap(key.currency1),
+            amount1,
+            address(this)
+        );
 
         // Approve tokens to pool
-        IERC20(Currency.unwrap(key.currency0)).approve(address(poolManager), amount0);
-        IERC20(Currency.unwrap(key.currency1)).approve(address(poolManager), amount1);
+        IERC20(Currency.unwrap(key.currency0)).approve(
+            address(poolManager),
+            amount0
+        );
+        IERC20(Currency.unwrap(key.currency1)).approve(
+            address(poolManager),
+            amount1
+        );
 
         // add liquidity to pool
         liquidityAdded = LiquidityAmounts.getLiquidityForAmounts(
@@ -169,7 +227,7 @@ contract Hook is BaseHook {
             TickMath.getSqrtPriceAtTick(tickLower),
             TickMath.getSqrtPriceAtTick(tickUpper),
             amount0,
-            amount1 
+            amount1
         );
 
         poolManager.modifyLiquidity(
@@ -194,10 +252,12 @@ contract Hook is BaseHook {
         bytes calldata hookData
     ) external override onlyPoolManager returns (bytes4, int128) {
         PoolId poolId = key.toId();
-        (uint160 sqrtPriceX96, int24 currentTick, , ) = poolManager.getSlot0(poolId);
+        (uint160 sqrtPriceX96, int24 currentTick, , ) = poolManager.getSlot0(
+            poolId
+        );
 
         if (sender == EL_AVS) {
-            if (currentTick < tickLower || currentTick > tickUpper) 
+            if (currentTick < tickLower || currentTick > tickUpper)
                 revert("Invalid tick by AVS");
             return (this.afterSwap.selector, 0);
         }
@@ -215,21 +275,38 @@ contract Hook is BaseHook {
         );
 
         // Calculate tokens amount
-        (uint256 amount0, uint256 amount1) = LiquidityAmounts.getAmountsForLiquidity(
-            sqrtPriceX96,
-            TickMath.getSqrtPriceAtTick(tickLower),
-            TickMath.getSqrtPriceAtTick(tickUpper),
-            liquidityAdded
-        );
+        (uint256 amount0, uint256 amount1) = LiquidityAmounts
+            .getAmountsForLiquidity(
+                sqrtPriceX96,
+                TickMath.getSqrtPriceAtTick(tickLower),
+                TickMath.getSqrtPriceAtTick(tickUpper),
+                liquidityAdded
+            );
 
         // approve tokens to lending protocol
-        IERC20(Currency.unwrap(key.currency0)).approve(address(lendingProtocol), amount0);
-        IERC20(Currency.unwrap(key.currency1)).approve(address(lendingProtocol), amount1);
+        IERC20(Currency.unwrap(key.currency0)).approve(
+            address(lendingProtocol),
+            amount0
+        );
+        IERC20(Currency.unwrap(key.currency1)).approve(
+            address(lendingProtocol),
+            amount1
+        );
 
         // Deposit tokens to lending protocol
-        lendingProtocol.deposit(Currency.unwrap(key.currency0), amount0);
-        lendingProtocol.deposit(Currency.unwrap(key.currency1), amount1);
-        
+        lendingProtocol.supply(
+            Currency.unwrap(key.currency0),
+            amount0,
+            address(this),
+            0
+        );
+        lendingProtocol.supply(
+            Currency.unwrap(key.currency1),
+            amount1,
+            address(this),
+            0
+        );
+
         return (this.afterSwap.selector, 0);
     }
 
@@ -241,8 +318,11 @@ contract Hook is BaseHook {
         BalanceDelta delta,
         bytes calldata
     ) external view onlyPoolManager returns (bytes4, BalanceDelta) {
-        if (sender != address(this))
-            revert("Add Liquidity thorugh Hook");
+        if (sender != address(this)) revert("Add Liquidity thorugh Hook");
         return (this.afterAddLiquidity.selector, delta);
+    }
+
+    function getATokenAddress(address asset) internal view returns (address) {
+        return lendingProtocol.getReserveData(asset).aTokenAddress;
     }
 }
